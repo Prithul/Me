7ab5a95a458f808c9608a6667cea9ba74fd8e4f7
Prithul <prithul28@gmail.com>
2019-01-03 22:18:06 -0500

    Again Commit!!!

--- src/me/Me.java
+++ src/me/Me.java
@@ -1,12 +1,28 @@
 package me;
 
 public class Me {
+
+    String sign;
+
+    public Me(){}
+    public Me(String sign){
+
+        this.sign = sign;
+    }
     public static void main(String[] args) {
 
         System.out.println("Exhausted!");
-
         System.out.println("ZZZ!!");
-
         System.out.println("Good Night!!!");
+
+        Me me = new Me(" :-) ");
+        me.howIsLife(" :-( ");
+    }
+
+    void howIsLife(String str){
+
+        String s = str;
+        System.out.println("How is Life ?  " + sign);
+        System.out.println("How is Life ? " + s);
     }
 }
